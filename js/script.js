0c8725b06f41f2596aa9e562e715dfa6f7e34be6
DimaSurdiy <dsurdiy@ukr.net>
2024-03-18 19:32:51 +0200

    added header

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
 "use strict"
+
+document.addEventListener("click", documentActions);
+
+function documentActions(e) {
+   const targetElement = e.target;
+
+   if (targetElement.closest(".menu-icon")) {
+      document.body.classList.toggle("menu-open");
+   }
+
+   if (targetElement.closest(".menu__link")) {
+      if (document.body.classList.contains("menu-open")) {
+         document.body.classList.remove("menu-open");
+      }
+   }
+}
