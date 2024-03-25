9317c83db4de7a395e2428f3b45283bac50ec2ce
DimaSurdiy <dsurdiy@ukr.net>
2024-03-25 21:04:24 +0200

    added blends section

--- js/script.js
+++ js/script.js
@@ -19,16 +19,28 @@ function documentActions(e) {
 // Sliders
 const heroSlider = document.querySelector(".hero-swiper");
 if (heroSlider) {
-   new Swiper('.hero-swiper', {
+   new Swiper(".hero-swiper", {
       direction: "vertical",
       loop: true,
       enabled: true,
       navigation: {
-         nextEl: '.hero-swiper__arrow--next',
-         prevEl: '.hero-swiper__arrow--prev',
+         nextEl: ".hero-swiper__arrow--next",
+         prevEl: ".hero-swiper__arrow--prev",
       },
       scrollbar: {
-         el: '.hero-swiper__scrollbar',
+         el: ".hero-swiper__scrollbar",
+      },
+   });
+}
+
+const blendsSlider = document.querySelector(".blends-swiper");
+if (blendsSlider) {
+   new Swiper(".blends-swiper", {
+      spaceBetween: 24,
+      slidesPerView: 3.7,
+      navigation: {
+         nextEl: ".blends-swiper__arrow--next",
+         prevEl: ".blends-swiper__arrow--prev",
       },
    });
 }
